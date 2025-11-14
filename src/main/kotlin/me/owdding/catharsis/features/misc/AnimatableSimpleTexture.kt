94a0e51a8fd96f1e4e2d87ea1fd44d735d4cd0e4
Gravy Boat <gravy@thatgravyboat.tech>
2025-11-14 14:31:44 -0330

    fix: ensure contents are closed and cleared before any possible errors

--- src/main/kotlin/me/owdding/catharsis/features/misc/AnimatableSimpleTexture.kt
+++ src/main/kotlin/me/owdding/catharsis/features/misc/AnimatableSimpleTexture.kt
@@ -29,17 +29,17 @@ class AnimatableSimpleTexture(location: ResourceLocation) : SimpleTexture(locati
 
     @Throws(IOException::class)
     override fun loadContents(manager: ResourceManager): TextureContents {
+        this.contents?.close()
+        this.ticker?.close()
+        this.ticker = null
+        this.contents = null
+
         val id = resourceId()
         val resource = manager.getResourceOrThrow(id)
         val image: NativeImage = resource.open().use(NativeImage::read)
         val textureMetadata = resource.metadata().getSection(TextureMetadataSection.TYPE).getOrNull()
         var animatedMetadata = resource.metadata().getSection(AnimationMetadataSection.TYPE).getOrNull()
         var frameSize = animatedMetadata?.calculateFrameSize(image.width, image.height) ?: FrameSize(image.width, image.height)
-
-        this.contents?.close()
-        this.ticker?.close()
-
-        this.ticker = null
 
         if (!Mth.isMultipleOf(image.width, frameSize.width) || !Mth.isMultipleOf(image.height, frameSize.height)) {
             logger.error("Image $id size ${image.width}x${image.height} is not a multiple of frame size ${frameSize.width}x${frameSize.height}")
@@ -67,14 +67,17 @@ class AnimatableSimpleTexture(location: ResourceLocation) : SimpleTexture(locati
 
         this.texture?.close()
         this.textureView?.close()
-        this.texture = device.createTexture(id::toString, 5, TextureFormat.RGBA8, this.contents?.width() ?: 0, this.contents?.height() ?: 0, 1, 1)
+        this.texture = device.createTexture(id::toString, 5, TextureFormat.RGBA8, this.contents?.width() ?: image.width, this.contents?.height() ?: image.height, 1, 1)
         this.textureView = device.createTextureView(this.texture!!)
 
         this.setFilter(blur, false)
         this.setClamp(clamp)
         this.contents?.uploadFirstFrame(0, 0, this.texture!!)
-
         this.ticker = this.contents?.createTicker()
+
+        if (this.contents == null) {
+            device.createCommandEncoder().writeToTexture(this.texture!!, image)
+        }
 
         if (this.ticker == null) {
             image.close() // Close the image if we don't need it anymore for animations
