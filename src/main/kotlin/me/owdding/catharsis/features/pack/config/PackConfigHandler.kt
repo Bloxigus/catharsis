377b85d08ada16ccfcaad8004fc15fd9eb2a8225
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-11-16 12:59:40 +0100

    fix: config saving
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/catharsis/features/pack/config/PackConfigHandler.kt
+++ src/main/kotlin/me/owdding/catharsis/features/pack/config/PackConfigHandler.kt
@@ -2,6 +2,7 @@ package me.owdding.catharsis.features.pack.config
 
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import kotlinx.coroutines.runBlocking
 import me.owdding.catharsis.utils.CatharsisLogger
 import me.owdding.ktmodules.Module
 import net.minecraft.util.GsonHelper
@@ -10,13 +11,21 @@ import tech.thatgravyboat.skyblockapi.api.events.base.predicates.TimePassed
 import tech.thatgravyboat.skyblockapi.api.events.time.TickEvent
 import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockapi.utils.Scheduling
+import tech.thatgravyboat.skyblockapi.utils.time.currentInstant
+import tech.thatgravyboat.skyblockapi.utils.time.since
+import java.util.concurrent.CompletableFuture
+import kotlin.io.path.createDirectories
+import kotlin.io.path.createFile
 import kotlin.io.path.notExists
 import kotlin.io.path.readText
 import kotlin.io.path.writeText
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Instant
+import kotlin.time.isDistantPast
 
 data class PackConfig(
     val default: JsonObject = JsonObject(),
-    val current: JsonObject = JsonObject()
+    val current: JsonObject = JsonObject(),
 ) {
 
     fun set(id: String, value: JsonElement) {
@@ -36,11 +45,16 @@ object PackConfigHandler {
     private val logger = CatharsisLogger.named("PackConfigHandler")
     private val path = McClient.config.resolve(SAVE_PATH)
     private val configs = mutableMapOf<String, PackConfig>()
-    private var saveRequestedAt = 0L
+    private var saveRequestedAt = Instant.DISTANT_PAST
 
     init {
         logger.runCatching("Loading pack configurations") {
-            if (path.notExists()) return@runCatching
+            if (path.notExists()) {
+                path.parent?.createDirectories()
+                path.createFile()
+                logger.info("No existing config found")
+                return@runCatching
+            }
             val json = GsonHelper.parse(path.readText())
             for ((key, value) in json.entrySet()) {
                 configs[key] = PackConfig(JsonObject(), value.asJsonObject)
@@ -53,13 +67,13 @@ object PackConfigHandler {
     }
 
     fun save() {
-        this.saveRequestedAt = System.currentTimeMillis()
+        this.saveRequestedAt = currentInstant()
     }
 
     @Subscription(TickEvent::class)
     @TimePassed("10s")
     fun onTick() {
-        if (saveRequestedAt != 0L && System.currentTimeMillis() - saveRequestedAt >= 10000L) {
+        if (!saveRequestedAt.isDistantPast && saveRequestedAt.since() >= 10.seconds) {
             val output = JsonObject()
             for ((key, value) in configs) {
                 output.add(key, value.current.deepCopy())
@@ -68,6 +82,7 @@ object PackConfigHandler {
             Scheduling.async {
                 logger.debug("Saving pack configurations to $SAVE_PATH")
                 path.writeText(output.toString())
+                saveRequestedAt = Instant.DISTANT_PAST
             }
         }
     }
