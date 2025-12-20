71932db67e5f505426dd5c40502638290b113f13
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-12-20 17:27:43 +0100

    fix: octree debug renderer rendering boxes that are too far away
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/catharsis/utils/types/boundingboxes/OctreeDebugRenderer.kt
+++ src/main/kotlin/me/owdding/catharsis/utils/types/boundingboxes/OctreeDebugRenderer.kt
@@ -30,19 +30,19 @@ object OctreeDebugRenderer {
         }
     }
 
-    fun visit(event: RenderWorldEvent, node: Node, nodesRendered: MutableInt, depth: Int, playerNode: Leaf?, playerPosition: BlockPos) {
+    fun visit(event: RenderWorldEvent, node: Node, nodesRendered: MutableInt, depth: Int, playerNode: Leaf?, playerPosition: BlockPos, renderDistance: Int = 25) {
         val aabb: AABB = node.getBox().toMinecraftAABB()
         if (
-            (min(
+            min(
                 abs(aabb.maxX - playerPosition.x),
                 abs(aabb.minX - playerPosition.x),
-            ) > 50 && min(
+            ) > renderDistance || min(
                 abs(aabb.maxY - playerPosition.y),
                 abs(aabb.minY - playerPosition.y),
-            ) > 50 && min(
+            ) > renderDistance || min(
                 abs(aabb.maxZ - playerPosition.z),
                 abs(aabb.minZ - playerPosition.z),
-            ) > 50) || aabb.contains(McPlayer)
+            ) > renderDistance || aabb.contains(McPlayer)
         ) return
         val color = (aabb.xsize / 16.0).roundToInt()
         val colorValue = color + 5L
