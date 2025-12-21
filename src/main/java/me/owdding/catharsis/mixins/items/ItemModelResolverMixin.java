7d0270ff1269a5a3a3c512d443c380ff6e8a2026
Gravy Boat <gravy@thatgravyboat.tech>
2025-12-21 17:16:36 -0330

    fix: carried slot models

--- src/main/java/me/owdding/catharsis/mixins/items/ItemModelResolverMixin.java
+++ src/main/java/me/owdding/catharsis/mixins/items/ItemModelResolverMixin.java
@@ -7,9 +7,11 @@ import me.owdding.catharsis.hooks.items.AbstractContainerScreenHook;
 import me.owdding.catharsis.hooks.items.ItemStackRenderStateHook;
 import me.owdding.catharsis.hooks.items.ModelManagerHook;
 import me.owdding.catharsis.utils.ItemUtils;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.item.ItemModelResolver;
 import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.world.entity.ItemOwner;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -29,11 +31,18 @@ public class ItemModelResolverMixin {
     }
 
     @ModifyExpressionValue(method = "appendItemLayers", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;get(Lnet/minecraft/core/component/DataComponentType;)Ljava/lang/Object;"))
-    private Object catharsis$modifyDataComponentType(Object original, @Local(argsOnly = true) ItemStack stack, @Local(argsOnly = true) ItemStackRenderState state) {
+    private Object catharsis$modifyDataComponentType(
+        Object original,
+        @Local(argsOnly = true) ItemStack stack,
+        @Local(argsOnly = true) ItemStackRenderState state,
+        @Local(argsOnly = true) ItemOwner owner
+    ) {
         if (manager == null) return original;
         if (state instanceof ItemStackRenderStateHook hook && !hook.catharsis$canFallthrough()) return original;
 
-        var guiId = GuiDefinitions.getSlot(AbstractContainerScreenHook.SLOT.get());
+        var isCarried = owner != null && owner.asLivingEntity() instanceof LocalPlayer player && player.containerMenu.getCarried() == stack;
+        var slot = AbstractContainerScreenHook.SLOT.get();
+        var guiId = isCarried || slot != null ? GuiDefinitions.getSlot(isCarried ? -1 : slot.index, stack) : null;
         var itemId = ItemUtils.INSTANCE.getCustomLocation(stack);
         var model = guiId != null ? guiId : itemId;
 
