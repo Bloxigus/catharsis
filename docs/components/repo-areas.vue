8e7627ed2a1e8c1f8696a1e2e8d2a002f667c6f4
Mona <59416038+meowora@users.noreply.github.com>
2025-12-23 23:32:33 +0100

    feat: add remote repo (#20)
    * feat: add remote repo
    
    * feat: update repo
    
    * feat: add 2 areas
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * feat: add gemstones
    
    * chore: add dwarven mines islands
    
    * feat: add repo area loading, and make catharsis namespace private
    
    * fix: octree hiding nodes the player is in
    
    * feat: add mithril titanium area
    
    * requested changes
    
    * mrow
    
    * feat: tungsten and umber
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * organize
    
    * mona stupid
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * restructure area repo
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * fix: missing gemstone islands
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * opal -> gemstone
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * feat: galatea trees
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * fix: 2 veins
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * feat: ch areas
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * feat: gt mithril
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * fix: not working on windows
    
    * feat: delete outdated files
    
    * glacite tunnels mithril
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * chore: bump sbapi 4.0.1
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * feat: add included areas
    
    * feat: add comments/descriptions
    
    * comments
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * chore: update description for ci gemstone
    
    * Update docs/data/repo-areas.data.ts
    
    Co-authored-by: ThatGravyBoat <gravy@thatgravyboat.tech>
    
    ---------
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    Co-authored-by: ThatGravyBoat <gravy@thatgravyboat.tech>

---
+++ docs/components/repo-areas.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { data } from "../data/repo-areas.data.ts";
+</script>
+<template>
+    <ul>
+        <li v-for="a in data">
+            <code>{{ a.name }}</code>
+            <span v-if="a.description !== undefined"> - {{ a.description}}</span>
+        </li>
+    </ul>
+</template>
