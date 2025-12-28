8b33f376b316e2356fe734fd29ba8545ba700add
Bloxigus <72960305+Bloxigus@users.noreply.github.com>
2025-12-28 14:32:28 +1030

    fix: animations now work

--- src/main/java/me/owdding/catharsis/mixins/entity/LivingEntityRendererMixin.java
+++ src/main/java/me/owdding/catharsis/mixins/entity/LivingEntityRendererMixin.java
@@ -1,6 +1,7 @@
 //~ named_identifier
 package me.owdding.catharsis.mixins.entity;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -18,6 +19,7 @@ import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
 import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
+import net.minecraft.client.renderer.state.CameraRenderState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,13 +28,31 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(LivingEntityRenderer.class)
-public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState, M extends EntityModel<? super S>>  {
+public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState, M extends EntityModel<? super S>> {
 
     @Shadow
     protected abstract boolean isBodyVisible(S renderState);
 
     @Shadow
     protected M model;
+
+    @WrapMethod(
+        method = "submit(Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/renderer/state/CameraRenderState;)V"
+    )
+    private void catharsis$swapOutModel(S renderState, PoseStack poseStack, SubmitNodeCollector nodeCollector, CameraRenderState cameraRenderState, Operation<Void> original) {
+        M originalModel = this.model;
+
+        var customModel = renderState.catharsis$getCustomEntityModel();
+
+        if (customModel != null && customModel.getModel() != null && model instanceof EntityModel<? super S> entityModel) {
+            //noinspection unchecked
+            this.model = (M) SafeModelPart.replaceModel(entityModel, renderState);
+        }
+
+        original.call(renderState, poseStack, nodeCollector, cameraRenderState);
+
+        this.model = originalModel;
+    }
 
     @WrapOperation(
         method = "submit(Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/renderer/state/CameraRenderState;)V",
@@ -41,20 +61,12 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extend
     public void catharsis$modifyEntityTexture(SubmitNodeCollector instance, Model<? super S> model, Object o, PoseStack poseStack, RenderType renderType, int i, int j, int k, TextureAtlasSprite textureAtlasSprite, int l, ModelFeatureRenderer.CrumblingOverlay crumblingOverlay, Operation<Void> original, @Local(argsOnly = true) S renderState) {
         var customModel = renderState.catharsis$getCustomEntityModel();
         var customRenderType = renderType;
-        var customEntityModel = model;
 
         if (customModel != null) {
-            if (customModel.getModel() != null && model instanceof EntityModel<? super S> entityModel) {
-                customEntityModel = SafeModelPart.replaceModel(entityModel, renderState);
-
-                // bad model :c
-                if (customEntityModel.getClass() != entityModel.getClass()) customEntityModel = entityModel;
-            }
-
             customRenderType = catharsis$createEntityTextureRenderType(renderState, customModel);
         }
 
-        original.call(instance, customEntityModel, o, poseStack, customRenderType, i, j, k, textureAtlasSprite, l, crumblingOverlay);
+        original.call(instance, model, o, poseStack, customRenderType, i, j, k, textureAtlasSprite, l, crumblingOverlay);
     }
 
     @WrapOperation(
@@ -71,13 +83,13 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extend
 
     @Unique
     private RenderType catharsis$createEntityTextureRenderType(S renderState, CustomEntityModel customEntityModel) {
-        boolean bodyVisible = isBodyVisible(renderState); // bl1
-        boolean idfk = !bodyVisible && !renderState.isInvisibleToPlayer; //bl2
+        boolean bodyVisible = isBodyVisible(renderState);
+        boolean spectatorVisible = !bodyVisible && !renderState.isInvisibleToPlayer;
         boolean isArmorStandMarker = renderState instanceof ArmorStandRenderState armorStandRenderState && armorStandRenderState.isMarker;
         var texture = customEntityModel.getTexture();
 
         if (isArmorStandMarker) {
-            if (idfk) {
+            if (spectatorVisible) {
                 return RenderTypes.entityTranslucent(texture, false);
             }
             if (bodyVisible) {
@@ -85,7 +97,7 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extend
             }
         }
 
-        if (idfk) {
+        if (spectatorVisible) {
             return RenderTypes.itemEntityTranslucentCull(texture);
         }
 
