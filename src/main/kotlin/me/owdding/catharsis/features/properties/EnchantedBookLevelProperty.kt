05da3365bd6ce4756b1e4b01d137d56353a1a2cb
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-12-29 11:05:31 +0100

    fix: enchantment level fallback
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/catharsis/features/properties/EnchantedBookLevelProperty.kt
+++ src/main/kotlin/me/owdding/catharsis/features/properties/EnchantedBookLevelProperty.kt
@@ -8,14 +8,16 @@ import net.minecraft.world.entity.ItemOwner
 import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
 import tech.thatgravyboat.skyblockapi.api.datatype.getData
+import tech.thatgravyboat.skyblockapi.api.remote.api.SkyBlockId.Companion.getSkyBlockId
+import tech.thatgravyboat.skyblockapi.utils.extentions.toFloatValue
 
 object EnchantedBookLevelProperty : RangeSelectItemModelProperty {
 
     val ID = Catharsis.id("enchanted_book_level")
-    val CODEC = MapCodec.unit { EnchantedBookLevelProperty }
+    val CODEC: MapCodec<EnchantedBookLevelProperty> = MapCodec.unit { EnchantedBookLevelProperty }
 
     override fun get(stack: ItemStack, level: ClientLevel?, owner: ItemOwner?, seed: Int): Float {
-        return stack.getData(DataTypes.ENCHANTMENTS).takeIf { it?.size == 1 }?.values?.firstOrNull()?.toFloat() ?: 0f
+        return stack.getData(DataTypes.ENCHANTMENTS).takeIf { it?.size == 1 }?.values?.firstOrNull()?.toFloat() ?: stack.getSkyBlockId()?.cleanId?.split(":")?.getOrNull(1).toFloatValue()
     }
 
     override fun type(): MapCodec<out RangeSelectItemModelProperty> = CODEC
