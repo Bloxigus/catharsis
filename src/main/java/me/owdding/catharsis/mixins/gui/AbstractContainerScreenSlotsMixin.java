e2b33832c9f4142f177d4966cd5caced0420830a
Bloxigus <72960305+Bloxigus@users.noreply.github.com>
2026-02-18 17:33:12 +1030

    idk even know

--- src/main/java/me/owdding/catharsis/mixins/gui/AbstractContainerScreenSlotsMixin.java
+++ src/main/java/me/owdding/catharsis/mixins/gui/AbstractContainerScreenSlotsMixin.java
@@ -15,7 +15,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ClickType;
 import net.minecraft.world.inventory.Slot;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,9 +26,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class AbstractContainerScreenSlotsMixin<T extends AbstractContainerMenu> extends Screen {
 
     @Shadow @Final protected T menu;
-
-    @Shadow
-    private @Nullable Slot clickedSlot;
 
     protected AbstractContainerScreenSlotsMixin(Component title) {
         super(title);
